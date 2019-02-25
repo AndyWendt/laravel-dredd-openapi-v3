3f77ade27e82cd3c2fbb11a46f891f808f218d09
Andy Wendt <andy@awendt.com>
2019-02-24 18:06:12 -0700

    Update docs test

--- bin/docs-test.sh
+++ bin/docs-test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Build the Docs
+./bin/docs-build.sh
+
+# Verify the Docs
 echo "DISABLE_MIDDLEWARE=true" >> .env
 ./node_modules/.bin/dredd --config ./tests/Dredd/v1/dredd.v1.yml
 sed -i '/DISABLE_MIDDLEWARE=true/d' .env
